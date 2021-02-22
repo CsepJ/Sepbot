cfc1d870d9fe8d70055eab0a272d42fe550ddca9
Sepbot_ <yoonjong32@gmail.com>
2021-02-22 00:32:02 +0000

    server font

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = express();
 server.all('/', (req, res)=>{
 
    res.setHeader('Content-Type', 'text/html');
-   res.write('<!DOCTYPE html><html><head><title>Server of Sepbot / Nyunbot</title><link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,300&display=swap" rel="stylesheet"></head><body>SP\'s discord bot is running</body></html>');
+   res.write('<!DOCTYPE html><html><head><title>Server of Sepbot / Nyunbot</title><link rel="preconnect" href="https://fonts.gstatic.com"><link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@1,300&display=swap" rel="stylesheet"><style>body{font-family:"Raleway", "sans-serif"}</style></head><body>SP\'s discord bot is running</body></html>');
    res.end();
 
 })
